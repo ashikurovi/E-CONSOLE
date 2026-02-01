323f69e1a5698899aa4f86fd0fe06731970526a7
Ashikur Rahman Ovi <ashikurovi2003@gmail.com>
2026-02-01 13:49:59 +0600

    update

--- src/pages/superadmin/package-components/PackageEditForm.jsx
+++ src/pages/superadmin/package-components/PackageEditForm.jsx
@@ -148,7 +148,7 @@ const PackageEditForm = ({ pkg, onClose }) => {
             price: parseFloat(data.price),
             discountPrice: data.discountPrice ? parseFloat(data.discountPrice) : null,
             isFeatured,
-            features,
+            features: validFeatures,
             ...(themeId && { themeId: parseInt(themeId) }),
         };
 
