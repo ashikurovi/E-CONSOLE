e5245eb6a7b6fb2f7a0206d1abdb27f0fae50c5e
Samsuddoha Sojib <Sojib@MacBook-Air.local>
2026-02-02 17:37:19 +0600

    style(media): update grid layout to 6 columns on xl screens

--- src/pages/media/index.jsx
+++ src/pages/media/index.jsx
@@ -302,8 +302,8 @@ export default function MediaPage() {
       </header>
 
       {/* --- CONTENT GRID --- */}
-      <main className="p-6 lg:p-10 max-w-[1600px] mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <main className="p-6 lg:p-10 max-w-[1920px] mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-6">
           {albums.map((album) => (
             <motion.div
               key={album.id}
