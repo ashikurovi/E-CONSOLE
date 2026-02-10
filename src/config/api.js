ea90d9265f6e5a3ac046282db5e5d768e97b3237
Ashikur Rahman Ovi <ashikurovi2003@gmail.com>
2026-02-10 16:01:27 +0600

    add custom domin

--- src/config/api.js
+++ src/config/api.js
@@ -12,7 +12,7 @@
 export const API_BASE_URL = 
   import.meta.env.VITE_API_BASE_URL || 
   import.meta.env.VITE_API_URL || 
-  "http://localhost:8000";
+  "https://squadcart-backend.up.railway.app";
   // https://squadcart-backend.up.railway.app
 // Export other API-related configs if needed
 export const API_CONFIG = {
